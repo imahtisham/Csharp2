1c5d505204c6f1e7bf07329db4bfd65ec1f09569
Muhammad Ehtisham Zafar <ahtizafi@gmail.com>
2024-06-14 19:31:07 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace conditions
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int k=20;
+        
+            Console.WriteLine("Enter Your Age");
+            Console.ReadLine();
+           if(k<20)
+          {
+            Console.WriteLine("You are eligible to caste a vote");
+            }
+            else
+            {
+                Console.WriteLine("Sorry Age Issue");
+            }
+          }
+            
+        }
+    }
